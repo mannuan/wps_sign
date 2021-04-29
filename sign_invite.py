9d5542ae33d365d985d787fa9c9425747185338c
mannuan <1271990125@qq.com>
2021-04-29 13:57:25 +0800

    Update sign_invite.py

--- sign_invite.py
+++ sign_invite.py
@@ -39,20 +39,7 @@ def push_wechat(txt):
 def main():
     # 被邀请人的 wps_sid 列表，
     sid_list = [
-        "V02StVuaNcoKrZ3BuvJQ1FcFS_xnG2k00af250d4002664c02f",
-        "V02SWIvKWYijG6Rggo4m0xvDKj1m7ew00a8e26d3002508b828",
-        "V02Sr3nJ9IicoHWfeyQLiXgvrRpje6E00a240b890023270f97",
-        "V02SBsNOf4sJZNFo4jOHdgHg7-2Tn1s00a338776000b669579",
-        "V02S2oI49T-Jp0_zJKZ5U38dIUSIl8Q00aa679530026780e96",
-        "V02ShotJqqiWyubCX0VWTlcbgcHqtSQ00a45564e002678124c",
-        "V02SFiqdXRGnH5oAV2FmDDulZyGDL3M00a61660c0026781be1",
-        "V02S7tldy5ltYcikCzJ8PJQDSy_ElEs00a327c3c0026782526",
-        "V02SPoOluAnWda0dTBYTXpdetS97tyI00a16135e002684bb5c",
-        "V02Sb8gxW2inr6IDYrdHK_ywJnayd6s00ab7472b0026849b17",
-        "V02SwV15KQ_8n6brU98_2kLnnFUDUOw00adf3fda0026934a7f",
-        "V02SC1mOHS0RiUBxeoA8NTliH2h2NGc00a803c35002693584d",
-        "V02S8fWiw86zsFg1oE5p4hCHAR6GPE000a4e69e2002508b828"
-
+        "V02SfUG1u_uadzjfgnFRJV_4dIEhu7Q00a7151250047f2345e",
     ]
     s = requests.session()
     invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
